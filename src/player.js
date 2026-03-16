7f194ad481bc1cc366110bbb66512f9c904c7a8e
LrnzZ <lrnzzian@gmail.com>
2026-03-16 18:00:30 +0800

    added lightning rod

--- src/player.js
+++ src/player.js
@@ -222,10 +222,10 @@ export class Player {
             this.fishIdleImg.src = `${folder}fishidle_green.png`;
         }else if (this.rodLevel === 4) {
             const folder = 'assets/rod/';
-            this.walkImg.src = `${folder}walk_gold.png`;
-            this.idleImg.src = `${folder}idle_gold.png`;
-            this.throwImg.src = `${folder}fullthrow_gold.png`;
-            this.fishIdleImg.src = `${folder}fishidle_gold.png`;
+            this.walkImg.src = `${folder}walk_blue.png`;
+            this.idleImg.src = `${folder}idle_blue.png`;
+            this.throwImg.src = `${folder}fullthrow_blue.png`;
+            this.fishIdleImg.src = `${folder}fishidle_blue.png`;
         }else if (this.rodLevel === 5) {
             const folder = 'assets/rod/';
             this.walkImg.src = `${folder}walk_legendary.png`;
